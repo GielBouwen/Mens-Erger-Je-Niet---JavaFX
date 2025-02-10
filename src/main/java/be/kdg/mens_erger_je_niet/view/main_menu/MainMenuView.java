ecfbd02dcc1220c5d10002cd21aa21da8e3a4b4c
GielBouwen <gb.gielbouwen@gmail.com>
2025-02-10 13:44:47 +0100

    Round buttons and colour

--- src/main/java/be/kdg/mens_erger_je_niet/view/main_menu/MainMenuView.java
+++ src/main/java/be/kdg/mens_erger_je_niet/view/main_menu/MainMenuView.java
@@ -23,19 +23,20 @@ public class MainMenuView extends BorderPane {
     }
 
     private void initializeNodes() {
-        double radius = 75;
+        double radius = 125;
+        double textSize = 30;
 
         newGame = new Button("New Game");
-        newGame.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        newGame.setFont(Font.font("Arial", FontWeight.BOLD, textSize));
         newGame.setTextFill(Color.WHITE);
-        newGame.setBackground(new Background(new BackgroundFill(Color.GOLD, new CornerRadii(radius), Insets.EMPTY)));
+        newGame.setBackground(new Background(new BackgroundFill(Color.GREEN, new CornerRadii(radius), Insets.EMPTY)));
         newGame.setPadding(new Insets(10));
         newGame.setShape(new Circle(radius));
         newGame.setMinSize((radius*2), (radius*2));
         newGame.setMaxSize((radius * 2), (radius * 2));
 
         loadGame = new Button("Load Game");
-        loadGame.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        loadGame.setFont(Font.font("Arial", FontWeight.BOLD, textSize));
         loadGame.setTextFill(Color.WHITE);
         loadGame.setBackground(new Background(new BackgroundFill(Color.GOLD, new CornerRadii(radius), Insets.EMPTY)));
         loadGame.setPadding(new Insets(10));
@@ -44,18 +45,18 @@ public class MainMenuView extends BorderPane {
         loadGame.setMaxSize((radius * 2), (radius * 2));
 
         help = new Button("Help");
-        help.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        help.setFont(Font.font("Arial", FontWeight.BOLD, textSize));
         help.setTextFill(Color.WHITE);
-        help.setBackground(new Background(new BackgroundFill(Color.GOLD, new CornerRadii(5), Insets.EMPTY)));
+        help.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(5), Insets.EMPTY)));
         help.setPadding(new Insets(10));
         help.setShape(new Circle(radius));
         help.setMinSize((radius*2), (radius*2));
         help.setMaxSize((radius * 2), (radius * 2));
 
         about = new Button("About");
-        about.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        about.setFont(Font.font("Arial", FontWeight.BOLD, textSize));
         about.setTextFill(Color.WHITE);
-        about.setBackground(new Background(new BackgroundFill(Color.GOLD, new CornerRadii(5), Insets.EMPTY)));
+        about.setBackground(new Background(new BackgroundFill(Color.DARKBLUE, new CornerRadii(5), Insets.EMPTY)));
         about.setPadding(new Insets(10));
         about.setShape(new Circle(radius));
         about.setMinSize((radius*2), (radius*2));
