3747c6609df386be6ac552671d7ec94b5f2f490a
GielBouwen <gb.gielbouwen@gmail.com>
2025-02-10 14:49:59 +0100

    Loading Screen and Image

--- src/main/java/be/kdg/mens_erger_je_niet/Main.java
+++ src/main/java/be/kdg/mens_erger_je_niet/Main.java
@@ -7,22 +7,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Stage primaryStage;
+
     @Override
     public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
         primaryStage.setTitle("Mens-Erger-Je-Niet");
         primaryStage.setWidth(1280);
         primaryStage.setHeight(720);
 
-        LoadingscreenView loadingscreenView = new LoadingscreenView(primaryStage);
+        showLoadingScreen();
+    }
+
+    private void showLoadingScreen() {
+        LoadingscreenView loadingscreenView = new LoadingscreenView(this);
         Scene loadingscreen = new Scene(loadingscreenView);
         primaryStage.setScene(loadingscreen);
+        primaryStage.show();
+    }
 
+    public void showMainMenu() {
         MainMenuView mainMenuView = new MainMenuView();
         Scene mainMenu = new Scene(mainMenuView);
         primaryStage.setScene(mainMenu);
-
-
-        primaryStage.show();
     }
 
     public static void main(String[] args) {
