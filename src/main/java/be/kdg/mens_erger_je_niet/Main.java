a6270fcc4d6a4c8e70a047c360093755a5a7c791
GielBouwen <gb.gielbouwen@gmail.com>
2025-02-11 15:04:22 +0100

    AboutView Ready

--- src/main/java/be/kdg/mens_erger_je_niet/Main.java
+++ src/main/java/be/kdg/mens_erger_je_niet/Main.java
@@ -53,7 +53,7 @@ public class Main extends Application {
     }
 
     public void aboutView() {
-        AboutView aboutView = new AboutView();
+        AboutView aboutView = new AboutView(this);
         Scene about = new Scene(aboutView);
         primaryStage.setScene(about);
     }
--- src/main/java/be/kdg/mens_erger_je_niet/view/about/AboutView.java
+++ src/main/java/be/kdg/mens_erger_je_niet/view/about/AboutView.java
@@ -1,7 +1,11 @@
 package be.kdg.mens_erger_je_niet.view.about;
 
+import be.kdg.mens_erger_je_niet.Main;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -10,36 +14,59 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class AboutView extends BorderPane {
+    private Label titleLabel;
+    private TextFlow aboutTextFlow;
     private Button GoBackButton;
-    private Text AboutText;
 
-    public AboutView() {
+
+    public AboutView(Main mainApp) {
         initializeNodes();
-        layoutNodes();
-    }
-    private void initializeNodes() {
-        double radius = 125;
-        double textSize = 30;
-        GoBackButton = new Button("Ga terug");
-        GoBackButton.setFont(Font.font("Arial", FontWeight.BOLD, textSize));
-        GoBackButton.setTextFill(Color.WHITE);
-        GoBackButton.setBackground(new Background(new BackgroundFill(Color.BLUE, new CornerRadii(radius), Insets.EMPTY)));
-        GoBackButton.setPadding(new Insets(10));
+        layoutNodes(mainApp);
     }
 
-    private void layoutNodes() {
-        AboutText = new Text("Dit is een lange tekst die gaat vertellen wie we zijn en wat we hier doen en ik maak deze tekst zo lang mogelijk om te zien hoe die uitgelijnd zou worden in het tekstvak en of die zou kunnen resizen of niet");
+    private void initializeNodes() {
+        titleLabel = new Label("ABOUT");
+        titleLabel.setFont(Font.font("Tahoma", FontWeight.BOLD, 30));
+
+
+        GoBackButton = new Button("Ga terug");
+        GoBackButton.setFont(Font.font("Tohoma", FontWeight.NORMAL, 30));
+        GoBackButton.setTextFill(Color.WHITE);
+        GoBackButton.setBackground(new Background(new BackgroundFill(Color.BLUE, new CornerRadii(125), Insets.EMPTY)));
+        GoBackButton.setPadding(new Insets(20));
+
+        Text AboutText = new Text("Deze applicatie is ontwikkeld door Wout en Giel in het kader van een " +
+                "programmeeropdracht. De opdracht was om een interactief spel te creëren, en wij hebben de klassieker " +
+                "Mens-Erger-Je-Niet, internationaal bekend als Ludo, toegewezen gekregen. Met veel toewijding en " +
+                "enthousiasme hebben we aan deze game gewerkt, waarbij we niet alleen de basisregels hebben " +
+                "geïmplementeerd, maar ook extra aandacht hebben besteed aan de gebruikservaring en het spelplezier. " +
+                "We hebben hier talloze uren aan besteed en hopen dat spelers net zoveel plezier beleven aan het spelen " +
+                "als wij aan het ontwikkelen ervan. Veel speelplezier!");
         AboutText.setFont(Font.font("Arial", 30));
 
-        TextFlow AboutTextFlow = new TextFlow(AboutText);
-        AboutTextFlow.setMaxWidth(500);
-        AboutText.setWrappingWidth(500);
+        aboutTextFlow = new TextFlow(AboutText);
+        aboutTextFlow.setMaxWidth(Double.MAX_VALUE);
+        aboutTextFlow.setPadding(new Insets(10));
+    }
 
-        GridPane tekst = new GridPane();
-        tekst.getChildren().add(AboutTextFlow);
+    private void layoutNodes(Main mainApp) {
+        VBox title = new VBox(10, titleLabel);
+        title.setPadding(new Insets(10));
+        title.setAlignment(Pos.TOP_CENTER);
 
-        this.setTop(AboutText);
-        //this.setBottom(GoBackButton);
-        //BorderPane.setAlignment(AboutText, Pos.TOP_CENTER);*/
+        VBox content = new VBox(10, aboutTextFlow);
+        content.setPadding(new Insets(10));
+        content.setAlignment(Pos.CENTER);
+
+        VBox goBackButton = new VBox(10, GoBackButton);
+        goBackButton.setPadding(new Insets(10));
+        goBackButton.setAlignment(Pos.CENTER);
+
+
+        this.setTop(title);
+        this.setCenter(content);
+        this.setBottom(goBackButton);
+
+        GoBackButton.setOnAction(event -> mainApp.showMainMenu());
     }
 }
