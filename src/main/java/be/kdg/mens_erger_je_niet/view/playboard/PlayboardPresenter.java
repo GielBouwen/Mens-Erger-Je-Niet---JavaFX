91d641e602af81e2eb50bb34c8d104c6277c9477
GielBouwen <gb.gielbouwen@gmail.com>
2025-03-16 18:49:04 +0100

    Updates Playboard  presenter

--- src/main/java/be/kdg/mens_erger_je_niet/view/playboard/PlayboardPresenter.java
+++ src/main/java/be/kdg/mens_erger_je_niet/view/playboard/PlayboardPresenter.java
@@ -12,7 +12,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
